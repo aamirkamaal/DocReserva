ef7c0cf0b4009da883077b768a8707ee4d5e0581
aamirkamaal <aamer@gtsinfosoft.com>
2024-06-06 21:23:34 +0530

    Login and register

---
+++ app/src/main/java/com/example/docreserva/helper/HelperClass.kt
@@ -0,0 +1,13 @@
+package com.example.docreserva.helper
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.colorResource
+import com.example.docreserva.R
+
+
+@Composable
+fun GetResource()
+{
+    val themeColor = colorResource(id = R.color.app_theme_color)
+
+}
---
+++ app/src/main/java/com/example/docreserva/ui/theme/CommanViewFile.kt
@@ -0,0 +1,8 @@
+package com.example.docreserva.ui.theme
+
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun AppLogo()
+{}
